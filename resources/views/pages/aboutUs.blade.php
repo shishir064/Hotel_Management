9bde1caf1cd3515b7a7147d4b22df86457a3da75
shishir064 <cdev4849.com>
2026-07-25 19:05:20 +0545

    add icons

--- resources/views/pages/aboutUs.blade.php
+++ resources/views/pages/aboutUs.blade.php
@@ -114,10 +114,16 @@
 
             <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
 
-                <div class="bg-white border  rounded-3xl p-8 text-center hover:shadow-lg transition">
+                <div
+                    class="bg-white border flex flex-col justify-center items-center rounded-3xl p-8 text-center hover:shadow-lg transition">
                     <div class="text-5xl mb-4 text-gray-700">
-                        <span class="material-symbols-outlined" style="font-size:60px; ">
-                            flash_on
+                        <span style="font-size:60px; ">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-15">
+                                <path fill-rule="evenodd"
+                                    d="M14.615 1.595a.75.75 0 0 1 .359.852L12.982 9.75h7.268a.75.75 0 0 1 .548 1.262l-10.5 11.25a.75.75 0 0 1-1.272-.71l1.992-7.302H3.75a.75.75 0 0 1-.548-1.262l10.5-11.25a.75.75 0 0 1 .913-.143Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+
                         </span>
                     </div>
                     <h3 class="text-xl font-bold mb-3">Fast Booking</h3>
@@ -126,10 +132,16 @@
                     </p>
                 </div>
 
-                <div class="bg-white border  rounded-3xl p-8 text-center hover:shadow-lg transition">
+                <div
+                    class="bg-white border flex flex-col justify-center items-center rounded-3xl p-8 text-center hover:shadow-lg transition">
                     <div class="text-5xl mb-4 text-gray-700">
-                        <span class="material-symbols-outlined" style="font-size:60px; ">
-                            lock
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-15">
+                                <path fill-rule="evenodd"
+                                    d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3c0-2.9-2.35-5.25-5.25-5.25Zm3.75 8.25v-3a3.75 3.75 0 1 0-7.5 0v3h7.5Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+
                         </span>
                     </div>
                     <h3 class="text-xl font-bold mb-3">Secure Payments</h3>
@@ -138,10 +150,17 @@
                     </p>
                 </div>
 
-                <div class="bg-white border  rounded-3xl p-8 text-center hover:shadow-lg transition">
+                <div
+                    class="bg-white border flex flex-col justify-between items-center rounded-3xl p-8 text-center hover:shadow-lg transition">
                     <div class="text-5xl mb-4 text-gray-700">
-                        <span class="material-symbols-outlined" style="font-size:60px; ">
-                            home
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-15">
+                                <path
+                                    d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                                <path
+                                    d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                            </svg>
+
                         </span>
                     </div>
                     <h3 class="text-xl font-bold mb-3">Quality Rooms</h3>
@@ -150,10 +169,17 @@
                     </p>
                 </div>
 
-                <div class="bg-white border  rounded-3xl p-8 text-center hover:shadow-lg transition">
+                <div
+                    class="bg-white border flex flex-col justify-between items-center rounded-3xl p-8 text-center hover:shadow-lg transition">
                     <div class="text-5xl mb-4 text-gray-700">
                         <span class="material-symbols-outlined" style="font-size:60px; ">
-                            mode_comment
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-15">
+                                <path
+                                    d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                                <path
+                                    d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                            </svg>
+
                         </span>
                     </div>
                     <h3 class="text-xl font-bold mb-3">24/7 Support</h3>
@@ -174,12 +200,12 @@
                 <div class="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
 
                     <div>
-                        <h3 class="text-5xl font-bold text-white">{{ $totalRoom}}</h3>
+                        <h3 class="text-5xl font-bold text-white">{{ $totalRoom }}</h3>
                         <p class="text-indigo-100 mt-2">Rooms Available</p>
                     </div>
 
                     <div>
-                        <h3 class="text-5xl font-bold text-white">{{ $totalUser}}</h3>
+                        <h3 class="text-5xl font-bold text-white">{{ $totalUser }}</h3>
                         <p class="text-indigo-100 mt-2">Happy Guests</p>
                     </div>
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,66 +1,74 @@
 <footer class="bg-gray-100 py-20 px-6 lg:px-20">
 
-        <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-10">
+    <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-10">
 
-            <div>
-                <h2 class="text-2xl mb-6 ">
-                    QuickStay
-                </h2>
+        <div>
+            <h2 class="text-2xl mb-6 ">
+                QuickStay
+            </h2>
 
-                <p class="text-gray-600 leading-relaxed">
-                    Defining the future of luxury travel through curated digital experiences and architectural wonder.
-                </p>
-            </div>
-
-            <div class="space-y-4">
-                <h5 class="uppercase text-sm font-semibold mb-4">
-                    Discover
-                </h5>
-
-                <a href="#" class="block text-gray-600 hover:text-black">The Collection</a>
-                <a href="#" class="block text-gray-600 hover:text-black">Destinations</a>
-                <a href="#" class="block text-gray-600 hover:text-black">Experiences</a>
-            </div>
-
-            <div class="space-y-4">
-                <h5 class="uppercase text-sm font-semibold mb-4">
-                    Company
-                </h5>
-
-                <a href="#" class="block text-gray-600 hover:text-black">About Us</a>
-                <a href="#" class="block text-gray-600 hover:text-black">Concierge</a>
-                <a href="#" class="block text-gray-600 hover:text-black">Sustainability</a>
-            </div>
-
-            <div class="space-y-4">
-                <h5 class="uppercase text-sm font-semibold mb-4">
-                    Legal
-                </h5>
-
-                <a href="#" class="block text-gray-600 hover:text-black">Privacy Policy</a>
-                <a href="#" class="block text-gray-600 hover:text-black">Terms of Service</a>
-            </div>
-        </div>
-
-        <div
-            class="max-w-7xl mx-auto border-t border-gray-300 mt-16 pt-8 flex flex-col md:flex-row justify-between items-center gap-4">
-
-            <p class="text-sm text-gray-500">
-                © 2024 QuickStay. Curated Sanctuary.
+            <p class="text-gray-600 leading-relaxed">
+                Defining the future of luxury travel through curated digital experiences and architectural wonder.
             </p>
-
-            <div class="flex gap-8">
-                <a href="#">
-                    <span class="material-symbols-outlined">
-                        public
-                    </span>
-                </a>
-
-                <a href="#">
-                    <span class="material-symbols-outlined">
-                        share
-                    </span>
-                </a>
-            </div>
         </div>
-    </footer>
+
+        <div class="space-y-4">
+            <h5 class="uppercase text-sm font-semibold mb-4">
+                Discover
+            </h5>
+
+            <a href="#" class="block text-gray-600 hover:text-black">The Collection</a>
+            <a href="#" class="block text-gray-600 hover:text-black">Destinations</a>
+            <a href="#" class="block text-gray-600 hover:text-black">Experiences</a>
+        </div>
+
+        <div class="space-y-4">
+            <h5 class="uppercase text-sm font-semibold mb-4">
+                Company
+            </h5>
+
+            <a href="#" class="block text-gray-600 hover:text-black">About Us</a>
+            <a href="#" class="block text-gray-600 hover:text-black">Concierge</a>
+            <a href="#" class="block text-gray-600 hover:text-black">Sustainability</a>
+        </div>
+
+        <div class="space-y-4">
+            <h5 class="uppercase text-sm font-semibold mb-4">
+                Legal
+            </h5>
+
+            <a href="#" class="block text-gray-600 hover:text-black">Privacy Policy</a>
+            <a href="#" class="block text-gray-600 hover:text-black">Terms of Service</a>
+        </div>
+    </div>
+
+    <div
+        class="max-w-7xl mx-auto border-t border-gray-300 mt-16 pt-8 flex flex-col md:flex-row justify-between items-center gap-4">
+
+        <p class="text-sm text-gray-500">
+            © 2026 QuickStay. Curated Sanctuary.
+        </p>
+
+        <div class="flex gap-8">
+            <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m6.115 5.19.319 1.913A6 6 0 0 0 8.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 0 0 2.288-4.042 1.087 1.087 0 0 0-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 0 1-.98-.314l-.295-.295a1.125 1.125 0 0 1 0-1.591l.13-.132a1.125 1.125 0 0 1 1.3-.21l.603.302a.809.809 0 0 0 1.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 0 0 1.528-1.732l.146-.292M6.115 5.19A9 9 0 1 0 17.18 4.64M6.115 5.19A8.965 8.965 0 0 1 12 3c1.929 0 3.716.607 5.18 1.64" />
+                    public
+                </svg>
+
+            </a>
+
+            <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
+                    share
+                </svg>
+
+            </a>
+        </div>
+    </div>
+</footer>
